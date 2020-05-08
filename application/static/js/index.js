6396b90a735436c6514c6f3eafc82888486e8263
kirill <tolkun_14@inbox.ru>
2020-05-08 20:47:30 +0300

    Add <v-menu> component to toolbar

--- application/static/js/index.js
+++ application/static/js/index.js
@@ -4,9 +4,42 @@ const toolbar = Vue.component('toolbar', {
             <v-toolbar class="elevation-0">
                 <v-toolbar-title>Photochnaja</v-toolbar-title>
                 <v-spacer></v-spacer>
+                <v-menu :offset-y="true">
+                    <template v-slot:activator="{ on }">
+                        <v-btn
+                            v-on="on"
+                            icon
+                        >
+                        <v-icon>mdi-dots-vertical</v-icon>
+                        </v-btn>
+                    </template>
+                    <v-list>
+                        <v-list-item
+                            v-for="(item, i) in menu.menuItems"
+                            v-bind:key="i"
+                            v-on:click.stop="item.method"
+                        >
+                            <v-list-item-title>{{ item.title }}</v-list-item-title>
+                        </v-list-item>
+                    </v-list>
+                </v-menu>
             </v-toolbar>
         </v-card>
         `,
+        data() {
+            return {
+                menu: {
+                    menuItems: [
+                        {title: 'About', method: this.menuAbout}
+                    ]
+                },
+                showAboutPopup: false
+            }
+        },
+        methods: {
+            menuAbout: function (event) {
+            },
+        }
     }
 )
 
