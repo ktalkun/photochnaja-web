d1ce5297fe9a5990597af27f063809b4af349e4d
kirill <tolkun_14@inbox.ru>
2020-05-08 20:39:33 +0300

    Create empty component 'toolbar'

--- application/static/js/index.js
+++ application/static/js/index.js
@@ -1,3 +1,15 @@
+const toolbar = Vue.component('toolbar', {
+        template: `
+        <v-card tile>
+            <v-toolbar class="elevation-0">
+                <v-toolbar-title>Photochnaja</v-toolbar-title>
+                <v-spacer></v-spacer>
+            </v-toolbar>
+        </v-card>
+        `,
+    }
+)
+
 var app = new Vue({
     el: '#app',
     vuetify: new Vuetify(),
